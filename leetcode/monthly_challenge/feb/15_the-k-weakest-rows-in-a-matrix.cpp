d8e7889d07a03206faf34f258c1db33763cc1e7a
ukohank517 <ukohank517@gmail.com>
2021-02-16 08:21:14 +0900

    [add] feb 15th

---
+++ leetcode/monthly_challenge/feb/15_the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <complex> // complex<double> a(1.2 , 2.3);// real(): 1.2, imag()2.3
+using namespace std;
+
+#define MOD 1000000007
+#define EPS 1e-10
+#define ll long long
+#define ld long double
+#define FOR(i,a,b) for(ll i=(ll)a;i<(ll)b;i++)
+#define rep(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rmsame(a) sort(all(a)),a.erase(unique(all(a)), a.end())
+#define rmvector(a,b) rep(i,a.size())rep(j,b.size())if(a[i]==b[j]){a.erase(a.begin()+i);i--;break;}
+#define first_more_idx(ve, num) upper_bound(all(ve), num)-ve.begin() // idx = first_more_idx(ve,num); if(idx = ve.size()) , then not exist
+#define first_lessequal_idx(ve, num) lower_bound(all(ve), num)-ve.begin()
+#define pq_pair_tB priority_queue <pair<ll,ll>, vector<pair<ll,ll> > , greater<pair<ll,ll> > >
+#define pq_pair_ts priority_queue <pair<ll,ll> > // only sort first param
+template<typename X> bool exist(vector<X> vec, X item){return find(all(vec), item)!=vec.end();}
+ll gcd(ll a, ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a, ll b){return a/gcd(a,b)*b;}
+ll power(ll x, ll n){if(n == 0) return 1;return  (n%2)? x*power(x, n-1)%MOD : power(x*x%MOD, n/2);}
+ll dx[]={0, 0, -1, 1};
+ll dy[]={1, -1, 0, 0};// up,down,left right
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int> >& mat, int k) {
+        map<ll, bool> chk;
+        vector<int> ans;
+
+        rep(j, mat[0].size()){
+            rep(i, mat.size()){
+                if(mat[i][j]==0){
+                    if(!chk[i]){
+                        chk[i] = true;
+                        ans.pb(i);
+                    }
+                }
+                if(ans.size() == k) break;
+            }
+            if(ans.size() == k) break;
+        }
+
+        ll idx = 0;
+        while(ans.size() != k){
+            if(!chk[idx]) ans.pb(idx);
+            idx ++;
+        }
+
+        return ans;
+    }
+};
